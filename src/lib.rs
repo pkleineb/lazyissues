60ebfff6618f61e62946e7745a6c194b87d15f7b
pkleineb <mail@paul-kleineberg.de>
2024-10-22 00:52:02 +0200

    feat(ui): ui stack manages ui and allows ez looping over them

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
+// started porting everything over to use traits so that i can generalise and also attribute the
+// key inputs to any panel; next up fix all the errors
+
 use std::{
-    fs::File,
     io,
-    io::Write,
     rc::Rc,
     result::Result,
     sync::mpsc,
-    thread,
     time::{Duration, Instant},
 };
 
-use graphql_requests::github::issue_query;
 use ratatui::{
     crossterm::{
         event::{self, Event as CrossEvent, KeyCode},
@@ -17,69 +16,21 @@ use ratatui::{
     },
     layout::{Constraint, Direction, Layout, Rect},
     prelude::CrosstermBackend,
-    style::{Color, Modifier, Style},
-    text::{Line, Span},
-    widgets::{Block, Borders, List, Tabs},
     Frame, Terminal,
 };
-use tokio::runtime::Runtime;
+use ui::UiStack;
 
 mod config;
 mod file_explorer;
 mod graphql_requests;
+mod tab_menu;
+mod ui;
 
 pub const TICK_RATE: Duration = Duration::from_millis(200);
 
 pub enum Event<I> {
     Input(I),
     Tick,
-}
-
-pub enum MenuItem {
-    Issues,
-    IssueView,
-    PullRequests,
-    PullRequestView,
-    Actions,
-    Projects,
-    ProjectsView,
-}
-
-impl From<&MenuItem> for usize {
-    fn from(input: &MenuItem) -> usize {
-        match input {
-            MenuItem::Issues | MenuItem::IssueView => 0,
-            MenuItem::PullRequests | MenuItem::PullRequestView => 1,
-            MenuItem::Actions => 2,
-            MenuItem::Projects | MenuItem::ProjectsView => 3,
-        }
-    }
-}
-
-impl From<&MenuItem> for String {
-    fn from(input: &MenuItem) -> String {
-        match input {
-            MenuItem::Issues | MenuItem::IssueView => "Issues".to_string(),
-            MenuItem::PullRequests | MenuItem::PullRequestView => "Pull requests".to_string(),
-            MenuItem::Actions => "Actions".to_string(),
-            MenuItem::Projects | MenuItem::ProjectsView => "Projects".to_string(),
-        }
-    }
-}
-
-impl MenuItem {
-    fn to_string_array() -> [String; 4] {
-        return [
-            "Issues".to_string(),
-            "Pull requests".to_string(),
-            "Actions".to_string(),
-            "Projects".to_string(),
-        ];
-    }
-}
-
-pub enum QueryData {
-    IssuesData(issue_query::ResponseData),
 }
 
 pub struct EventLoop {
@@ -141,8 +92,6 @@ impl EventLoop {
 
 pub struct TerminalApp {
     input_receiver: mpsc::Receiver<Event<CrossEvent>>,
-    query_receiver: mpsc::Receiver<QueryData>,
-    query_clone_sender: mpsc::Sender<QueryData>,
 
     terminal: Terminal<CrosstermBackend<io::Stdout>>,
 }
@@ -151,22 +100,12 @@ impl TerminalApp {
     pub fn new(input_receiver: mpsc::Receiver<Event<CrossEvent>>) -> Result<Self, std::io::Error> {
         let stdout = io::stdout();
         let backend = CrosstermBackend::new(stdout);
-        let terminal = Terminal::new(backend);
+        let terminal = Terminal::new(backend)?;
 
-        let (query_clone_sender, query_receiver): (
-            mpsc::Sender<QueryData>,
-            mpsc::Receiver<QueryData>,
-        ) = mpsc::channel();
-
-        match terminal {
-            Ok(terminal) => Ok(Self {
-                input_receiver,
-                query_receiver,
-                query_clone_sender,
-                terminal,
-            }),
-            Err(error) => Err(error),
-        }
+        Ok(Self {
+            input_receiver,
+            terminal,
+        })
     }
 
     pub fn run(&mut self) {
@@ -175,8 +114,6 @@ impl TerminalApp {
             return;
         }
 
-        let mut active_menu_item = MenuItem::Issues;
-        let mut query_response_data: Vec<QueryData> = Vec::new();
         let mut config = match config::read_config() {
             Ok(Some(config)) => config,
             Ok(None) => config::Config::new(),
@@ -186,47 +123,55 @@ impl TerminalApp {
             }
         };
 
-        let explorer = file_explorer::FileExplorer::new();
+        let mut ui_stack = Self::create_ui(UiStack::new());
 
         loop {
             let _ = self.terminal.draw(|render_frame| {
                 let layout = Self::create_base_layout(render_frame);
-                Self::create_tab_menu(render_frame, layout[0], &active_menu_item);
 
-                match active_menu_item {
-                    MenuItem::Issues => {
-                        Self::render_issues_view(render_frame, layout[1], &query_response_data)
-                    }
-                    _ => (),
-                }
-
-                if config.is_default() {
-                    explorer.render(render_frame, layout[1])
+                for panel in ui_stack.iter() {
+                    panel.render(render_frame, &layout)
                 }
             });
 
             match self.input_receiver.recv() {
-                Ok(event) => {
-                    let close = self.handle_input(event, &mut active_menu_item);
-                    if close {
-                        break;
+                Ok(event) => match event {
+                    Event::Input(event) => match event {
+                        CrossEvent::Key(key) => match key.code {
+                            KeyCode::Char('q') => {
+                                self.clean_up_terminal(None);
+                                break;
+                            }
+                            _ => (), // use event here
+                        },
+                        _ => (),
+                    },
+                    Event::Tick => {
+                        for panel in ui_stack.iter_rev() {
+                            panel.tick();
+                        }
                     }
-                }
+                },
                 Err(error) => {
-                    self.clean_up_terminal(Some(format!("{error} occured during receiving input!")))
+                    self.clean_up_terminal(Some(format!(
+                        "{error} occured during receiving input!"
+                    )));
+                    break;
                 }
             };
-
-            // try_recv does not block the current thread which is nice here because we don't
-            // have a tick signal recv() would block the thread until we receive a message from
-            // the sender I am ignoring the error here but that may not be best practice
-            if let Ok(data) = self.query_receiver.try_recv() {
-                // this vector might get quite big so I need to implement garbage collection which
-                // looks if there is two of the same QueryData since we can then delete the older
-                // one
-                query_response_data.push(data);
-            }
         }
+    }
+
+    fn create_ui(mut ui_stack: UiStack) -> UiStack {
+        let tab_menu = tab_menu::TabMenu::new(0);
+        ui_stack.add_panel(tab_menu, 10);
+
+        match file_explorer::FileExplorer::new(1) {
+            Ok(explorer) => ui_stack.add_panel(explorer, 0),
+            Err(error) => println!("{error} occured during creation of file explorer!"),
+        }
+
+        ui_stack
     }
 
     fn create_base_layout(render_frame: &mut Frame) -> Rc<[Rect]> {
@@ -235,134 +180,6 @@ impl TerminalApp {
             .direction(ratatui::layout::Direction::Vertical)
             .constraints([Constraint::Length(3), Constraint::Min(2)].as_ref())
             .split(size)
-    }
-
-    fn create_tab_menu(render_frame: &mut Frame, chunk: Rect, active_menu_item: &MenuItem) {
-        let menu_string_items = MenuItem::to_string_array();
-        let menu: Vec<Line> = menu_string_items
-            .iter()
-            .map(|title| {
-                let (first, rest) = title.split_at(1);
-                Line::from(vec![
-                    Span::styled(
-                        first,
-                        Style::default()
-                            .fg(Color::Red)
-                            .add_modifier(Modifier::UNDERLINED),
-                    ),
-                    Span::styled(rest, Style::default().fg(Color::White)),
-                ])
-            })
-            .collect();
-
-        let tabs = Tabs::new(menu)
-            .select(active_menu_item.into())
-            .block(
-                Block::default()
-                    .title(String::from(active_menu_item))
-                    .borders(Borders::ALL),
-            )
-            .style(Style::default().fg(Color::White))
-            .highlight_style(Style::default().fg(Color::Red))
-            .divider(Span::raw("|"));
-
-        render_frame.render_widget(tabs, chunk);
-    }
-
-    fn ask_for_token_files(render_frame: &mut Frame, chunk: Rect, config: &mut config::Config) {}
-
-    fn render_issues_view(
-        render_frame: &mut Frame,
-        chunk: Rect,
-        query_response_data: &Vec<QueryData>,
-    ) {
-        for response_data in query_response_data.iter() {
-            match response_data {
-                QueryData::IssuesData(data) => {
-                    if let Some(repo) = &data.repository {
-                        Self::display_issues(&repo, render_frame, chunk)
-                    }
-                }
-            }
-        }
-    }
-
-    fn display_issues(
-        repo_data: &issue_query::IssueQueryRepository,
-        render_frame: &mut Frame,
-        chunk: Rect,
-    ) {
-        if let Some(issues) = &repo_data.issues.nodes {
-            let issue_items: Vec<Span> = issues
-                .iter()
-                .map(|issue| {
-                    if let Some(node) = issue {
-                        return Span::styled(&node.title, Style::default());
-                    }
-                    Span::default()
-                })
-                .collect();
-
-            let issue_list = List::new(issue_items)
-                .block(Block::default().title("Issues").borders(Borders::ALL))
-                .style(Style::default().fg(Color::White));
-
-            render_frame.render_widget(issue_list, chunk);
-        }
-    }
-
-    fn handle_input(&mut self, event: Event<CrossEvent>, active_menu_item: &mut MenuItem) -> bool {
-        match event {
-            Event::Input(event) => match event {
-                CrossEvent::Key(key) => match key.code {
-                    KeyCode::Char('q') => {
-                        self.clean_up_terminal(None);
-                        return true;
-                    }
-                    KeyCode::Char('I') => {
-                        *active_menu_item = MenuItem::Issues;
-                        let cloned_sender = self.query_clone_sender.clone();
-                        thread::spawn(move || {
-                            let runtime = Runtime::new();
-                            match runtime {
-                                Ok(runtime) => runtime.block_on(async {
-                                    Self::fetch_issues(cloned_sender).await;
-                                }),
-                                Err(error) => println!("{error} occured while creating runtime"),
-                            };
-                        });
-                    }
-                    KeyCode::Char('P') => *active_menu_item = MenuItem::PullRequests,
-                    KeyCode::Char('A') => *active_menu_item = MenuItem::Actions,
-                    KeyCode::Char('r') => *active_menu_item = MenuItem::Projects,
-                    _ => (),
-                },
-                _ => (),
-            },
-            Event::Tick => (),
-        }
-
-        false
-    }
-
-    async fn fetch_issues(sender: mpsc::Sender<QueryData>) {
-        let variables = graphql_requests::github::issue_query::Variables {
-            repo_name: "test_repo".to_string(),
-            repo_owner: "pkleineb".to_string(),
-        };
-
-        let response_data = graphql_requests::github::perform_issue_query(variables).await;
-
-        match response_data {
-            Ok(ok) => match ok {
-                Some(data) => match sender.send(QueryData::IssuesData(data)) {
-                    Err(error) => println!("{error} occured during sending of query data!"),
-                    _ => (),
-                },
-                None => println!("No data fetched from server!"),
-            },
-            Err(error) => println!("{:?} occured during fetching data from server!", error),
-        };
     }
 
     fn clean_up_terminal(&mut self, message: Option<String>) {
---
+++ src/ui.rs
@@ -0,0 +1,33 @@
+use std::{collections::BTreeMap, rc::Rc};
+
+use ratatui::{crossterm::event::KeyCode, layout::Rect, Frame};
+
+pub trait PanelElement {
+    fn handle_input(&mut self, key_code: KeyCode) -> bool;
+    fn render(&mut self, render_frame: &mut Frame, layout: &Rc<[Rect]>) -> ();
+    fn tick(&mut self) -> ();
+}
+
+pub struct UiStack {
+    panels: BTreeMap<u8, Box<dyn PanelElement>>,
+}
+
+impl UiStack {
+    pub fn new() -> Self {
+        Self {
+            panels: BTreeMap::new(),
+        }
+    }
+
+    pub fn add_panel<P: PanelElement + 'static>(&mut self, panel: P, priority: u8) {
+        self.panels.insert(priority, Box::new(panel));
+    }
+
+    pub fn iter(&mut self) -> impl Iterator<Item = &mut Box<dyn PanelElement>> {
+        self.panels.values_mut()
+    }
+
+    pub fn iter_rev(&mut self) -> impl Iterator<Item = &mut Box<dyn PanelElement>> {
+        self.panels.values_mut().rev()
+    }
+}
