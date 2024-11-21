c23ea2577b134d45204721a7bff6058518a1bdc4
pkleineb <mail@paul-kleineberg.de>
2024-11-22 00:20:05 +0100

    feat(file_explorer): file explorer can go down dirs and you can filter by just typing(although capital letters don't work yet)

--- src/ui/file_explorer.rs
+++ src/ui/file_explorer.rs
@@ -3,6 +3,7 @@ use std::{
     io::{self, Write},
     path::PathBuf,
     rc::Rc,
+    time::{Duration, Instant},
 };
 
 use ratatui::{
@@ -17,10 +18,15 @@ use crate::{create_floating_layout, ui::PanelElement};
 
 pub struct FileExplorer {
     current_path: PathBuf,
+    path_mask: String,
     items: Vec<PathBuf>,
     state: ListState,
 
     layout_position: usize,
+
+    cursor_flicker_delay: Duration,
+    last_cursor_flicker: Instant,
+    cursor_rendered_last_flicker: bool,
 }
 
 impl FileExplorer {
@@ -28,10 +34,15 @@ impl FileExplorer {
         let current_path = std::env::current_dir()?;
         let mut explorer = Self {
             current_path,
+            path_mask: String::from(""),
             items: Vec::new(),
             state: ListState::default(),
 
             layout_position,
+
+            cursor_flicker_delay: Duration::from_millis(300),
+            last_cursor_flicker: Instant::now(),
+            cursor_rendered_last_flicker: false,
         };
         explorer.update_items()?;
         Ok(explorer)
@@ -47,7 +58,9 @@ impl FileExplorer {
     fn update_items(&mut self) -> io::Result<()> {
         self.items = fs::read_dir(&self.current_path)?
             .filter_map(|entry| entry.ok().map(|entry| entry.path()))
+            .filter(|entry| self.compare_entry_to_mask(entry.to_str().unwrap_or_default()))
             .collect();
+
         self.items.sort();
         self.items.insert(0, "..".into());
         self.state.select(Some(0));
@@ -58,8 +71,11 @@ impl FileExplorer {
         match self.state.selected() {
             Some(selected) => {
                 let path = &self.items[selected];
-                if path.is_dir() {
+                if path.to_str().unwrap_or_default() == ".." {
+                    self.go_down_dir()?;
+                } else if path.is_dir() {
                     self.current_path = path.clone();
+                    self.clear_mask();
                     self.update_items()?;
                 }
                 Ok(())
@@ -95,6 +111,77 @@ impl FileExplorer {
         };
         self.state.select(Some(entry_index));
     }
+
+    fn compare_entry_to_mask(&self, entry: &str) -> bool {
+        if entry.contains(
+            &(self.current_path.to_str().unwrap_or_default().to_owned() + "/" + &self.path_mask),
+        ) {
+            return true;
+        }
+
+        false
+    }
+
+    fn add_to_mask(&mut self, char: char) -> io::Result<()> {
+        self.path_mask += &char.to_string();
+
+        self.update_items()?;
+        Ok(())
+    }
+
+    fn remove_from_mask(&mut self) -> io::Result<()> {
+        if self.path_mask.len() == 0 {
+            self.go_down_dir()?;
+            return Ok(());
+        }
+
+        self.path_mask.remove(self.path_mask.len() - 1);
+
+        self.update_items()?;
+        Ok(())
+    }
+
+    fn go_down_dir(&mut self) -> io::Result<()> {
+        match self.current_path.parent() {
+            Some(parent_path) => {
+                let new_mask = self
+                    .current_path
+                    .to_str()
+                    .unwrap_or_default()
+                    .split("/")
+                    .last()
+                    .unwrap_or_default();
+                self.path_mask = String::from(new_mask);
+
+                self.current_path = parent_path.to_path_buf();
+            }
+            _ => (),
+        }
+
+        self.update_items()?;
+
+        Ok(())
+    }
+
+    fn clear_mask(&mut self) {
+        self.path_mask.clear();
+    }
+
+    fn render_cursor(&mut self) -> &str {
+        let should_switch_mode =
+            Instant::now() - self.last_cursor_flicker > self.cursor_flicker_delay;
+
+        if should_switch_mode {
+            self.cursor_rendered_last_flicker = !self.cursor_rendered_last_flicker;
+            self.last_cursor_flicker = Instant::now();
+        }
+
+        if self.cursor_rendered_last_flicker {
+            return "_";
+        } else {
+            return " ";
+        }
+    }
 }
 
 impl PanelElement for FileExplorer {
@@ -107,6 +194,14 @@ impl PanelElement for FileExplorer {
                 KeyCode::Tab => self.next_entry(),
                 KeyCode::Enter => match self.enter_dir() {
                     Err(error) => println!("{error} occured during switching directory!"),
+                    _ => (),
+                },
+                KeyCode::Char(char) => match self.add_to_mask(char) {
+                    Err(error) => println!("{error} occured during adding to mask!"),
+                    _ => (),
+                },
+                KeyCode::Backspace => match self.remove_from_mask() {
+                    Err(error) => println!("{error} occured during removing from mask!"),
                     _ => (),
                 },
                 _ => (),
@@ -131,7 +226,12 @@ impl PanelElement for FileExplorer {
             .highlight_style(Style::default().bg(Color::DarkGray))
             .block(
                 Block::default()
-                    .title(self.current_path.to_str().unwrap_or("path"))
+                    .title(
+                        self.current_path.to_str().unwrap_or("path").to_owned()
+                            + "/"
+                            + &self.path_mask
+                            + self.render_cursor(),
+                    )
                     .borders(Borders::ALL),
             )
             .style(Style::default().fg(Color::White));
