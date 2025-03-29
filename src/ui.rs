bd4424dd406916b851fd7607c042da1ab293b677
pkleineb <mail@paul-kleineberg.de>
2025-03-29 02:33:48 +0100

    feat(ui_stack): added utility functions for removing panels based on priority. This might need to be reworked in the future

--- src/ui.rs
+++ src/ui.rs
@@ -27,6 +27,24 @@ impl UiStack {
         self.panels.insert(priority, Box::new(panel));
     }
 
+    pub fn remove_panel(&mut self, priority: u8) -> Option<Box<dyn PanelElement>> {
+        self.panels.remove(&priority)
+    }
+
+    pub fn remove_highest_priority_panel(&mut self) -> Option<Box<dyn PanelElement>> {
+        if let Some((&priority, _)) = self.panels.last_key_value() {
+            return self.panels.remove(&priority);
+        }
+        None
+    }
+
+    pub fn remove_lowest_priority_panel(&mut self) -> Option<Box<dyn PanelElement>> {
+        if let Some((&priority, _)) = self.panels.first_key_value() {
+            return self.panels.remove(&priority);
+        }
+        None
+    }
+
     pub fn get_highest_priority(&self) -> u8 {
         self.panels
             .last_key_value()
