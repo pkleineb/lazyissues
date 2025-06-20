4ab633c05004115e0b5596c43eb653f99350bfed
pkleineb <mail@paul-kleineberg.de>
2025-06-20 23:33:53 +0200

    feat(detail_view): implemented basic rendering of comments

--- src/ui/detail_view.rs
+++ src/ui/detail_view.rs
@@ -4,7 +4,7 @@ use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Line, Span},
-    widgets::{Block, Borders, Paragraph},
+    widgets::{Block, Borders, Paragraph, Wrap},
     Frame,
 };
 
@@ -20,7 +20,7 @@ pub trait DetailItem: std::fmt::Debug {
     fn get_num_comments(&self) -> usize;
 
     /// returns all fetched comments on the trait
-    fn get_comments(&self) -> Vec<Box<dyn Comment>>;
+    fn get_comments(&self) -> Vec<&dyn Comment>;
 }
 
 /// trait for comments
@@ -80,6 +80,49 @@ impl DetailView {
 
         render_frame.render_widget(spacer_bar, vertical_split[1]);
     }
+
+    fn render_body(item: &dyn Comment, render_frame: &mut Frame, area: Rect) {
+        let block = Block::default()
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::Gray));
+
+        let inner_area = block.inner(area);
+        render_frame.render_widget(block, area);
+
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(1), Constraint::Fill(1)])
+            .split(inner_area);
+
+        let title = format!(
+            "{} commented on {}",
+            item.get_author_login().unwrap_or_default(),
+            item.get_created_at()
+        );
+
+        let title_paragraph = Paragraph::new(Span::styled(title, Style::default()));
+        render_frame.render_widget(title_paragraph, layout[0]);
+
+        let body_paragraph = Paragraph::new(Span::styled(item.get_body(), Style::default()))
+            .wrap(Wrap { trim: false });
+        render_frame.render_widget(body_paragraph, layout[1]);
+    }
+
+    fn calculate_body_height(text: &str, width: usize) -> usize {
+        let mut lines = 0;
+
+        for paragraph in text.lines() {
+            if paragraph.is_empty() {
+                lines += 1;
+                continue;
+            }
+
+            let line_amount = paragraph.len().div_ceil(width);
+            lines += line_amount;
+        }
+
+        lines
+    }
 }
 
 impl PanelElement for DetailView {
@@ -90,7 +133,49 @@ impl PanelElement for DetailView {
             return;
         };
 
-        Self::render_title(unwrapped_item.deref(), render_frame, rect);
+        let padding = 10;
+        let padded_width = rect.width - 2 * padding;
+
+        let main_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(2), Constraint::Fill(1)])
+            .split(rect);
+
+        Self::render_title(unwrapped_item.deref(), render_frame, main_layout[0]);
+
+        let center_comment_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Fill(1),
+                Constraint::Length(padded_width),
+                Constraint::Fill(1),
+            ])
+            .split(main_layout[1]);
+
+        let mut comments = unwrapped_item.get_comments();
+        comments.insert(0, unwrapped_item.deref() as &dyn Comment);
+
+        let constraints: Vec<_> = comments
+            .iter()
+            .map(|comment| {
+                Constraint::Length(
+                    Self::calculate_body_height(comment.get_body(), (padded_width + 2).into()) // +2 for the borders left and right
+                        as u16
+                        + 1  // +1 for the title where created at and author goes and 
+                        + 2, // +2 for the borders up and down
+                )
+            })
+            .collect();
+
+        let comment_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(constraints)
+            .spacing(1)
+            .split(center_comment_layout[1]);
+
+        for (comment, area) in comments.iter().zip(comment_layout.iter()) {
+            Self::render_body(*comment, render_frame, *area);
+        }
     }
 
     fn update(&mut self, data: RepoData) -> bool {
